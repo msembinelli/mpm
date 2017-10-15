c57475cbb82016d99be2e42eb858889236a9b51c
Matthew Sembinelli <matthew.sembinelli@gmail.com>
2017-10-15 02:23:26 -0600

    Increase version to 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='mpm',
-    version='0.1',
+    version='0.2',
     py_modules=['mpm_cli', 'mpm', 'yaml_storage'],
     install_requires=[
         'click',
